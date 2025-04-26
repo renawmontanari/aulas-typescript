0f101c328dbd3ee58a669c4f4d337daef829516f
renawmontanari <regiangelica@gmail.com>
2025-04-26 15:08:48 -0300

    finalizado a criacao da view mensagem

--- app/views/mensagem-view.ts
+++ app/views/mensagem-view.ts
@@ -1,1 +1,17 @@
-export class MensagemView {}
+export class MensagemView {
+  private elemento: HTMLElement;
+  constructor(seletor: string) {
+    this.elemento = document.querySelector(seletor);
+  }
+
+  template(model: string): string {
+    return `
+        <p class="alert alert-info">${model}</p>
+    `;
+  }
+
+  update(model: string): void {
+    const template = this.template(model);
+    this.elemento.innerHTML = template;
+  }
+}
