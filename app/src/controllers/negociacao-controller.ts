73d67cb1ae33d5b714120895787928fd8e7785d6
renawmontanari <regiangelica@gmail.com>
2025-05-02 19:42:17 -0300

    criando decorator de propriedades

--- app/src/controllers/negociacao-controller.ts
+++ app/src/controllers/negociacao-controller.ts
@@ -1,3 +1,4 @@
+import { inspect } from "../decorators/inspecionar.js";
 import { logarTempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js";
 import { DiasDaSemana } from "../enums/dias-da-semana.js";
 import { Negociacao } from "../models/negociacao.js";
@@ -6,22 +7,21 @@ import { MensagemView } from "../views/mensagem-view.js";
 import { NegociacoesView } from "../views/negociacoes-view.js";
 
 export class NegociacaoController {
+  @domInject("#data")
   private inputData: HTMLInputElement;
+  @domInject("#quantidade")
   private inputQuantidade: HTMLInputElement;
+  @domInject("#valor")
   private inputValor: HTMLInputElement;
   private negociacoes = new Negociacoes();
   private negociacoesView = new NegociacoesView("#negociacoesView");
   private mensagemView = new MensagemView("#mensagemView");
 
   constructor() {
-    this.inputData = <HTMLInputElement>document.querySelector("#data");
-    this.inputQuantidade = document.querySelector(
-      "#quantidade"
-    ) as HTMLInputElement;
-    this.inputValor = document.querySelector("#valor") as HTMLInputElement;
     this.negociacoesView.update(this.negociacoes);
   }
 
+  @inspect
   @logarTempoDeExecucao()
   public adiciona(): void {
     const negociacao = Negociacao.criaDe(
