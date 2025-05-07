3307e85a2bc47f6c0610856467faf351ab99385e
renawmontanari <regiangelica@gmail.com>
2025-05-07 19:01:59 -0300

    evitando importar negociacoes duplicadas

--- app/dist/js/controllers/negociacao-controller.js
+++ app/dist/js/controllers/negociacao-controller.js
@@ -34,7 +34,16 @@ export class NegociacaoController {
         this.atualizaView();
     }
     importarDados() {
-        this.negociacaoService.obterNegociacoesDoDia().then((negociacoesDeHoje) => {
+        this.negociacaoService
+            .obterNegociacoesDoDia()
+            .then((negociacoesDeHoje) => {
+            return negociacoesDeHoje.filter((negociacoesDeHoje) => {
+                return !this.negociacoes
+                    .lista()
+                    .some((negociacao) => negociacao.ehIgual(negociacoesDeHoje));
+            });
+        })
+            .then((negociacoesDeHoje) => {
             for (let negociacao of negociacoesDeHoje) {
                 this.negociacoes.adiciona(negociacao);
             }
